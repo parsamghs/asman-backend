ba3e97133ce2f0e299d2bbbf37357787bf3afead
Parsa <parsapy2005@gmail.com>
2025-08-11 13:44:23 +0330

    update logs

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -8,21 +8,20 @@ function getClientIp(req) {
 }
 
 function requestLogger(req, res, next) {
-  const start = process.hrtime.bigint(); // شروع زمان
+  const start = process.hrtime.bigint();
 
   res.on('finish', async () => {
     const durationNs = process.hrtime.bigint() - start;
-    const durationMs = Number(durationNs) / 1e6; // میلی‌ثانیه
+    const durationMs = Number(durationNs) / 1e6;
 
     const ip = getClientIp(req);
 
-    // اگر IP لوکال بود، موقعیت نگذار
     if (
       ip.startsWith('10.') || ip.startsWith('192.168.') || ip.startsWith('172.') ||
       ip.startsWith('127.') || ip.startsWith('100.') || ip === '::1'
     ) {
       logger.info(
-        `${req.method} ${req.originalUrl} ${res.statusCode} - ${res.get('content-length') || 0} bytes - ${durationMs.toFixed(2)} ms - IP ${ip} - Location: Local/Private IP`
+        `${req.method} ${req.originalUrl} ${res.statusCode}`
       );
       return;
     }
@@ -31,19 +30,19 @@ function requestLogger(req, res, next) {
       const { data } = await axios.get(`https://ipwho.is/${ip}`);
 
       if (data.success) {
-        const location = `${data.city}, ${data.region}, ${data.country} (${data.connection?.org || 'Unknown ISP'})`;
+        const location = `${data.city}, ${data.region}, ${data.country}`;
         logger.info(
-          `${req.method} ${req.originalUrl} ${res.statusCode} - ${res.get('content-length') || 0} bytes - ${durationMs.toFixed(2)} ms - IP ${ip} - Location: ${location}`
+          `${req.method} ${req.originalUrl} ${res.statusCode} - IP ${ip} - Location: ${location}`
         );
       } else {
         logger.info(
-          `${req.method} ${req.originalUrl} ${res.statusCode} - ${res.get('content-length') || 0} bytes - ${durationMs.toFixed(2)} ms - IP ${ip} - Location: Unknown`
+          `${req.method} ${req.originalUrl} ${res.statusCode} - IP ${ip} - Location: Unknown`
         );
       }
     } catch (err) {
       logger.warn(`GeoIP error for IP ${ip}: ${err.message}`);
       logger.info(
-        `${req.method} ${req.originalUrl} ${res.statusCode} - ${res.get('content-length') || 0} bytes - ${durationMs.toFixed(2)} ms - IP ${ip} - Location: GeoIP Error`
+        `${req.method} ${req.originalUrl} ${res.statusCode} - IP ${ip} - Location: GeoIP Error`
       );
     }
   });
