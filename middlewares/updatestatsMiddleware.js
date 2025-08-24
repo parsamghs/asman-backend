36097b9e21a4f48fb3806d9774bde07fa1d784cf
Parsa <parsapy2005@gmail.com>
2025-08-24 19:11:21 +0330

    fix middleware 3

--- middlewares/updatestatsMiddleware.js
+++ middlewares/updatestatsMiddleware.js
@@ -1,20 +1,23 @@
 const pool = require('../db');
+const moment = require('moment-timezone');
 
 const UpdateStats = async (req, res, next) => {
   try {
-    console.log("UpdateStats user.id:", req.user?.id);
-
     if (req.user && req.user.id) {
+      const userId = Number(req.user.id);
+
+      const tehranTime = moment().tz("Asia/Tehran").format('YYYY-MM-DD HH:mm:ss');
+
       await pool.query(
         `INSERT INTO users_stats (id, last_active)
-         VALUES ($1, NOW())
+         VALUES ($1, $2)
          ON CONFLICT (id)
          DO UPDATE SET last_active = EXCLUDED.last_active`,
-        [req.user.id]
+        [userId, tehranTime]
       );
     }
   } catch (err) {
-    console.error('Error updating last_active:', err);
+    console.error('Error updating last_active:', err.message);
   }
   next();
 };
