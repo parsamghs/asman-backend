7724d9b8e0ac97ce2f9eec21d81dffd2f2c143ae
Parsa <parsapy2005@gmail.com>
2025-08-24 18:10:57 +0330

    test stats middleware

--- middlewares/updatestatsMiddleware.js
+++ middlewares/updatestatsMiddleware.js
@@ -3,6 +3,7 @@ const pool = require('../db');
 const UpdateStats = async (req, res, next) => {
   try {
     if (req.user && req.user.id) {
+      console.log("UpdateStats user.id:", req.user.id);
       await pool.query(
         `INSERT INTO users_stats (id, last_active)
          VALUES ($1, NOW())
@@ -12,9 +13,10 @@ const UpdateStats = async (req, res, next) => {
       );
     }
   } catch (err) {
-    console.error('Error updating last_active:', err);}
+    console.error('Error updating last_active:', err);
+  }
   next();
-  };
-
+};
+console.log("UpdateStats user.id:", req.user?.id);
 
 module.exports = UpdateStats;
