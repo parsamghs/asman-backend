2dc366f11bdf27f4c8c3bf3fa5e1b3801078a1be
Parsa Maghsoudi <parsapy2005@gmail.com>
2025-11-15 14:56:28 +0330

    add receptionist to lost orders

--- src/modules/follow-parts/routes/OrdersCentralRoute.js
+++ src/modules/follow-parts/routes/OrdersCentralRoute.js
@@ -30,30 +30,29 @@ const authAdminOrWarehouse = [authMiddleware, dealerAccessMiddleware, roleMiddle
 const authAllOrdersRoles = [authMiddleware, dealerAccessMiddleware, roleMiddleware('انباردار','مدیریت','پذیرش','حسابدار'), UpdateStats];
 const authUpdateOrderRoles = [authMiddleware, dealerAccessMiddleware, roleMiddleware('انباردار', 'حسابدار','پذیرش','مدیریت'), UpdateStats];
 const authUpdateMultipleStatusRoles = [authMiddleware, dealerAccessMiddleware, roleMiddleware('انباردار','مدیریت','پذیرش','حسابدار'), UpdateStats];
-const authLostOrdersRoles = [authMiddleware, dealerAccessMiddleware, roleMiddleware('انباردار','مدیریت'), UpdateStats];
 const homedashboardmiddlewares =  [authMiddleware, roleMiddleware('انباردار','مدیریت','پذیرش','حسابدار'), UpdateStats];
 
 router.get('/all', authAllOrdersRoles, getAllOrders);
 router.get('/get-cars', authAllOrdersRoles, getAllCars);
-router.get('/getlostorders', authLostOrdersRoles, getLostOrders);
+router.get('/getlostorders', authUpdateMultipleStatusRoles, getLostOrders);
 router.get('/orders-count', homedashboardmiddlewares, getOrderscounts);
 router.get('/search-orders', authAllOrdersRoles, searchOrders);
 router.get('/partname-suggest/:partname_id', authAdminOrWarehouse, suggestPartsByName);
 router.get('/suggest-parts', authAdminOrWarehouse, suggestParts);
 router.get('/search-lost-orders', authAdminOrWarehouse, searchLostOrders);
 
-router.post('/addlostorder', authAdminOrWarehouse, addLostOrder);
+router.post('/addlostorder', authUpdateMultipleStatusRoles, addLostOrder);
 router.post('/add', authAdminOrWarehouse, addOrder);
 router.post('/add-pieces-to-customer/:customer_id', authAdminOrWarehouse, addPiecesToCustomer);
 router.post('/add-pieces-to-reception/:reception_id', authAdminOrWarehouse, addPiecesToExistingReception);
 
 router.patch('/bulkedit', authUpdateMultipleStatusRoles, updateMultipleOrderStatus);
 router.patch('/edit-customer/:customerId', authUpdateOrderRoles, updateOrder);
-router.patch('/update-lost-order/:id', authLostOrdersRoles, updateLostOrder);
+router.patch('/update-lost-order/:id', authUpdateMultipleStatusRoles, updateLostOrder);
 router.patch('/edit/:orderId', authUpdateOrderRoles, updatestatus);
 
 router.delete('/deletecustomer/:customerId', authAdminOrWarehouse, deleteCustomerAndAllOrders);
-router.delete('/delete-lost-order/:id', authAdminOrWarehouse, deleteLostOrder);
+router.delete('/delete-lost-order/:id', authUpdateMultipleStatusRoles, deleteLostOrder);
 router.delete('/deleteorder/:order_id', authAdminOrWarehouse, deleteOrderPiece);
 
 
